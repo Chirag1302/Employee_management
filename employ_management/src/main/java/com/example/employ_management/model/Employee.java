efc79fade7c26f9b47d802913e963017dc774802
Chirag <143170337+Chirag1302@users.noreply.github.com>
2026-06-09 00:18:43 +0530

    Specify MongoDB collection name for Employee

--- employ_management/src/main/java/com/example/employ_management/model/Employee.java
+++ employ_management/src/main/java/com/example/employ_management/model/Employee.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
-@Document
+@Document(collection = "employees")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
